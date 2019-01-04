55bc83675c62c5329719a095c7d3c50cac19da8c
gravel93 <agaevruslan223@gmail.com>
2019-01-03 21:48:35 -0500

    Updated project added DataDrivenTest

--- generic/src/main/java/main/java/BrowserDriver.java
+++ generic/src/main/java/main/java/BrowserDriver.java
@@ -97,18 +97,18 @@ public class BrowserDriver {
         public WebDriver getLocalDriver(String browserName, String os) {
             if (browserName.equalsIgnoreCase("chrome")) {
                 if (os.equalsIgnoreCase("windows")) {
-                    System.setProperty("webdriver.chrome.driver", "../generic/driver/chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver", "../generic/drivers/chromedriver.exe");
                     driver = new ChromeDriver();
                 } else if (os.equalsIgnoreCase("mac")) {
-                    System.setProperty("webdriver.chrome.driver", "../generic/driver/chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver", "../generic/drivers/chromedriver");
                     driver = new ChromeDriver();
                 }
             } else if (browserName.equalsIgnoreCase("firefox")) {
                 if (os.equalsIgnoreCase("windows")) {
-                    System.setProperty("webdriver.gecko.driver", "../generic/driver/geckodriver.exe");
+                    System.setProperty("webdriver.gecko.driver", "../generic/drivers/geckodriver.exe");
                     driver = new ChromeDriver();
                 } else if (os.equalsIgnoreCase("mac")) {
-                    System.setProperty("webdriver.gecko.driver", "../generic/driver/geckodriver.exe");
+                    System.setProperty("webdriver.gecko.driver", "../generic/drivers/geckodriver");
                     driver = new ChromeDriver();
                 }
             }
